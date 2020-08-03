2504f2a783d8d2a5eb81207ada56c23eb70cc540
Daynamarie <Dayna.christison@gmail.com>
2020-08-03 11:09:29 -0400

    Data--

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 
-import './App.css';
-import Header from './Header'
-
+import "./App.css";
+import Header from "./Header";
+import axios from "axios";
 
 function App() {
+  const [records, updateRecords] = useState([]);
+  const [fetchRecords, invokeFetch] = useState(true);
+
+  useEffect(() => {
+    const getAirtableRecords = async () => {
+      const response = await axios.get(
+        `https://api.airtable.com/v0/appBipVvhjiI1uNnZ/Patient?maxRecords=3&view=Main%20View`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
+          },
+        }
+      );
+      updateRecords(response.data);
+      console.log(response);
+    };
+    getAirtableRecords();
+  }, [fetchRecords]);
+
   return (
     <div className="App">
       <Header />
