19ea98758e26ac154bd22a61e23881237af72abd
Daynamarie <Dayna.christison@gmail.com>
2020-08-04 15:15:29 -0400

    hot pink buttons and nav alignments

--- src/components/ServiceTotals.js
+++ src/components/ServiceTotals.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import ServiceDetails from "./ServiceDetails";
 
 const ServiceTotals = (props) => {
-  
-  const { records } = props; 
+  const { records } = props;
   const [currentRecords, updateCurrentRecords] = useState([]);
   console.log(props);
 
@@ -18,22 +17,23 @@ const ServiceTotals = (props) => {
   return (
     <div>
       <h1> Overall Service Totals </h1>
-      <button onClick={() => setCurrentFilter("Blood work")}>Blood work</button>
-      <button onClick={() => setCurrentFilter("Cardiology")}>Cardiology</button>
-      <button onClick={() => setCurrentFilter("Check-up")}>Check ups</button>
-      <button onClick={() => setCurrentFilter("Consultation")}>
-        Consultations
-      </button>
-      <button onClick={() => setCurrentFilter("Radiology")}>
-        Radiology
-      </button>
+      <div className="service-buttons">
+        <button onClick={() => setCurrentFilter("Blood work")}>
+          Blood work
+        </button>
+        <button onClick={() => setCurrentFilter("Cardiology")}>
+          Cardiology
+        </button>
+        <button onClick={() => setCurrentFilter("Check-up")}>Check ups</button>
+        <button onClick={() => setCurrentFilter("Consultation")}>
+          Consultations
+        </button>
+        <button onClick={() => setCurrentFilter("Radiology")}>Radiology</button>
+      </div>
       <div>
         <h2>{currentRecords.service}</h2>
         {currentRecords.map((record) => (
-            <ServiceDetails
-                record={record}
-                key={record.id}
-            />
+          <ServiceDetails record={record} key={record.id} />
         ))}
       </div>
     </div>
