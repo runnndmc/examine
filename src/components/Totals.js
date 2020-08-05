769c3444b913c590298f360e2d6f09f1cc898ded
Daynamarie <Dayna.christison@gmail.com>
2020-08-05 11:40:54 -0400

    still owe displaying

--- src/components/Totals.js
+++ src/components/Totals.js
@@ -4,10 +4,13 @@ const Totals = (props) => {
     const { bill } = props;
     console.log(bill)
 
+    const billsTotal = bill.fields.stillowe
+
     return (
         <div>
             {bill.fields.service}
             <h1>this is your total</h1>
+            <h2>here: {billsTotal}</h2>
         </div>
     )
 }
